feaa6937cc8adeb15326bf962024b41d7bcf64de
장훈 <gelheart1011@gmail.com>
2014-09-23 12:15:20 +0900

    초기소스

---
+++ J20140923/src/생성자.java
@@ -0,0 +1,20 @@
+
+public class 생성자 {
+	String name;
+	public 생성자() {//기본생성자	
+		this("SSS");
+		System.out.println("기본생성자");
+		this.name = "AAA";
+	}
+	public 생성자(String name)	{
+		this("SSS", 10);
+		System.out.println("생성자"+name);
+	}
+	public 생성자(String name, int a)	{
+		this("SSS", 10, 100.5d);
+		System.out.println("생성자"+name+ " "+a);
+	}
+	public 생성자(String name, int a , double b)	{
+		System.out.println("생성자"+name+ " "+a +" "+b);
+	}
+}
